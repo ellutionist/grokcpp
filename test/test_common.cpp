03d442d7eff23ad5acdc3853b9d90f21700c8fd9
ellutionist <impc53802956@163.com>
2022-07-31 07:13:21 +0800

    [GROKCPP-2] test: add test cases for common module

--- test/test_common.cpp
+++ test/test_common.cpp
@@ -1,5 +1,22 @@
 #include "common.h"
 #include "gtest/gtest.h"
+#include <string>
+#include <unordered_map>
+
+TEST(COMMON, FORMAT) {
+  std::string s = grok::string_format(
+      "a string: %s, an integer: %03d, a float: %.2f", "string", 25, 0.4321);
+
+  EXPECT_EQ(s, "a string: string, an integer: 025, a float: 0.43");
+}
+
+TEST(COMMON, CONTAINS) {
+  std::unordered_map<std::string, std::string> map;
+  map.insert({"valid_key", "value"});
+
+  EXPECT_EQ(grok::contains(map, std::string("valid_key")), true);
+  EXPECT_EQ(grok::contains(map, std::string("invalid_key")), false);
+}
 
 TEST(COMMON, SPLIT) {
   auto parts = grok::split("one;two;three", ";");
@@ -8,3 +25,5 @@ TEST(COMMON, SPLIT) {
   parts = grok::split("how are you", " ", 2);
   EXPECT_EQ(parts, decltype(parts)({"how", "are you"}));
 }
+
+TEST(COMMON, FILE_NAME_MACRO) { EXPECT_EQ(__FILE_NAME__, "test_common.cpp"); }
